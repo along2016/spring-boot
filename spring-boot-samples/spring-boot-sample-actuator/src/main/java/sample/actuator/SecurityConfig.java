da91b733b0b11853f40be82d3af339055a73b77b
dtwangjiliang <845506513@qq.com>
2020-07-18 16:07:51 +0800

    防止POST请求报 403

---
+++ spring-boot-samples/spring-boot-sample-actuator/src/main/java/sample/actuator/SecurityConfig.java
@@ -0,0 +1,14 @@
+package sample.actuator;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// 这个方法可以解决 POST 请求403的错误
+		http.csrf().disable();
+	}
+}
